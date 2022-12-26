322bb7118de3bdc3aca63f718961df30398fcd24
Ivan <ivan.turutin@gmail.com>
2022-12-26 16:01:32 +0300

    Initialization

---
+++ db/scripts/001_ddl_create_tasks_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS tasks
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL UNIQUE,
+  description TEXT NOT NULL,
+  created TIMESTAMP NOT NULL,
+  done BOOLEAN NOT NULL
+);
+
+comment on table tasks is 'Задачи';
+comment on column tasks.id is 'Идентификатор задачи';
+comment on column tasks.name is 'Название задачи';
+comment on column tasks.description is 'Описание задачи';
+comment on column tasks.created is 'Время создания задачи';
+comment on column tasks.done is 'Индикатор выполнения задачи';
