62bd430407edebec7588cdccd2b2625ac0741b67
Ivan <ivan.turutin@gmail.com>
2022-12-28 20:49:08 +0300

    Добавил javaDocs в SimpleTaskService

--- src/main/java/ru/job4j/todo/service/SimpleTaskService.java
+++ src/main/java/ru/job4j/todo/service/SimpleTaskService.java
@@ -9,6 +9,9 @@ import ru.job4j.todo.repository.TaskRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Сервисный слой задач
+ */
 @ThreadSafe
 @Service
 public class SimpleTaskService implements TaskService {
@@ -19,36 +22,72 @@ public class SimpleTaskService implements TaskService {
         this.repository = repository;
     }
 
+    /**
+     * Обрабатывает запрос при добавлении задачи в репозиторий
+     * @param task добавляемая задача
+     * @return задачу обернутую в Optional если добавление задачи прошло успешно, и Optional.empty() если добавить
+     * задачу не удалось
+     */
     @Override
     public Optional<Task> add(Task task) {
         return repository.add(task);
     }
 
+    /**
+     * Обрабатывает запрос при поиске задачи по идентификатору
+     * @param id идентификатор задачи
+     * @return задачу обернутую в Optional если задача найдена, и Optional.empty() если не найдена
+     */
     @Override
     public Optional<Task> findById(int id) {
         return repository.findById(id);
     }
 
+    /**
+     * Обрабатывает запрос при поиске всех задач
+     * @return список всех найденных задач
+     */
     @Override
     public List<Task> findAll() {
         return repository.findAll();
     }
 
+    /**
+     * Обрабатывает запрос при изменении задачи
+     * @param task задача, которую требуется изменить
+     * @return true если задача обновлена, false если не обновлена
+     */
     @Override
     public boolean update(Task task) {
         return repository.update(task);
     }
 
+    /**
+     * Обрабатывает запрос при удалении задачи
+     * @param task задача, которую необходимо удалить
+     * @return задачу обернутую в Optional если задача удалена, и Optional.empty() если не удалена
+     */
     @Override
     public Optional<Task> delete(Task task) {
         return Optional.empty();
     }
 
+    /**
+     * Обрабатывает запрос при обновлении статуса задачи
+     * @param task задача, которую требуется изменить
+     * @return true если задача обновлена, false если не обновлена
+     */
     @Override
     public boolean updateDone(Task task) {
         return repository.updateDone(task);
     }
 
+    /**
+     * Обрабатывает запрос при поиске задач в которых поле done соответствует аргументу
+     * @param isDone аргумент, по которому определяется какие задачи требуется искать true - выполненные,
+     *               false - не выполненные
+     * @return список задач соответствующих запросу
+     */
     @Override
     public List<Task> findByDone(boolean isDone) {
         return repository.findByDone(isDone);
